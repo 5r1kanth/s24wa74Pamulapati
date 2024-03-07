2e7e5883721f31f144022c31fcf99fdee64e4d2f
5r1kanth <S560174@nwmissouri.edu>
2024-03-07 17:39:18 -0600

    Part 4 - Assignment 9

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,9 @@
-var express = require('Srikanth Pamulapati');
+var express = require('express');
 var router = express.Router();
 
 /* My Data Page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Express' });
+  res.render('mydata', { title: 'Srikanth Pamulapati' });
 });
 
 module.exports = router;
